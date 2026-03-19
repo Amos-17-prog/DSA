9642f393f2c1260bba6548e40367e59954a3b027
Amos-17-prog <tharuamos012@gmail.com>
2026-03-19 16:20:34 +0545

    Hooks

---
+++ basicViteReact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./Chai"
+function App() {
+
+  const username = 'chaiAurCode'
+
+  return (
+    <>
+      <Chai />
+      <h1>Hello world! {username} </h1>
+    </>
+  )
+}
+
+export default App
