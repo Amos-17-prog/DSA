5c09d9002fecebea3c9fce84bd4d0c1e6c0d566d
Amos-17-prog <tharuamos012@gmail.com>
2026-03-30 16:32:16 +0545

    Currency Changer

--- basicViteReact/src/main.jsx
+++ basicViteReact/src/main.jsx
@@ -1,39 +1,10 @@
-import React, { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
 
-function myApp() {
-  return (
-    <div>
-      <h1>Custom App | Chai</h1>
-    </div>
-  )
-}
-
-// const ReactElement = {
-//     type: 'a',
-//     props: {
-//         href: 'https://google.com',
-//         target: '_blank',
-//     },
-//     children: 'Click me to visit google',
-// }
-
-const anotherElement = (
-  <a href="https://google.com" target='_blank'>Visit google</a>
-)
-
-const anotherUser = "chai aur code"
-
-const ReactElement = React.createElement(
-  'a',
-  { href: 'https://google.com', target: '_black' },
-  'Click me to visit google',
-  anotherUser
-)
-
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  <App />
-  // </StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
